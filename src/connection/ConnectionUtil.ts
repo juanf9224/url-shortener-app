77ecd623f8cf3d07bdd101cbdfd109065719512b
juanf9224 <juanfh.24.6@gmail.com>
2019-05-05 03:37:19 -0400

    - update tsconfig file - update connection util to use cloud provider

--- src/connection/ConnectionUtil.ts
+++ src/connection/ConnectionUtil.ts
@@ -1,17 +1,17 @@
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const user = process.env.DB_REMOTE_USER || 'admin';
 const pwd = process.env.DB_REMOTE_PWD || 'admin';
-const db = process.env.DB_REMOTE || 'test?retryWrites=true';
+const db = process.env.DB_REMOTE || 'shortener-db?retryWrites=true';
 const url = process.env.DB_REMOTE_URL || '@powerfull-api-mongodb-cluster-gbmsd.mongodb.net/';
 const connectionString = `mongodb+srv://${user}:${pwd}${url}${db}`;
 const connectionStringLocal = process.env.DB_CONNECTION_STRING || '';
 
 const ConnectionUtil = {
-    mongooseConnect: () => mongoose.connect(connectionStringLocal, 
+    mongooseConnect: () => mongoose.connect(connectionString, 
         { 
             useNewUrlParser: true,
             useFindAndModify: false,
